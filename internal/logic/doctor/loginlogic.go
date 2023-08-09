33581b1dc9c64f3b1b52b83b2c7f6b3d640e1907
west <xfeiyv@gmail.com>
2023-08-09 23:54:39 +0800

    增加登录逻辑

--- internal/logic/doctor/loginlogic.go
+++ internal/logic/doctor/loginlogic.go
@@ -2,6 +2,11 @@ package doctor
 
 import (
 	"context"
+	"google.golang.org/grpc/status"
+	"meg-backup-gozero/common/cryptx"
+	"meg-backup-gozero/common/jwtx"
+	"meg-backup-gozero/models/doctor"
+	"time"
 
 	"meg-backup-gozero/internal/svc"
 	"meg-backup-gozero/internal/types"
@@ -24,7 +29,25 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 }
 
 func (l *LoginLogic) Login(req *types.LoginRequest) (resp *types.LoginResponse, err error) {
-	// todo: add your logic here and delete this line
+	res, err := l.svcCtx.DoctorModel.FindOneByUsername(l.ctx, req.Username)
+	if err != nil {
+		if err == doctor.ErrNotFound {
+			return nil, status.Error(100, "user not found")
+		}
+		return nil, status.Error(500, err.Error())
+	}
+	if res.Password != cryptx.PasswordEncrypt(l.svcCtx.Config.Salt, req.Password) {
+		return nil, status.Error(100, "password error")
+	}
+	now := time.Now().Unix()
+	accessExpire := l.svcCtx.Config.Auth.AccessExpire
+	accessToken, err := jwtx.GetToken(l.svcCtx.Config.Auth.AccessSecret, now, accessExpire, res.Id)
+	if err != nil {
+		return nil, err
+	}
 
-	return
+	return &types.LoginResponse{
+		AccessToken:  accessToken,
+		AccessExpire: accessExpire,
+	}, nil
 }
